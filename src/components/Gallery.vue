98e6d3ebf71dac22998e347142a9576b8d79a416
Sabrina Markon <phpsitescripts@outlook.com>
2018-08-30 00:23:52 -0600

    Fix eslint indentation and other errors. Set up webpack with bootstrap-loader

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="gallery">
-        <h1>{{ message }}</h1>
-    </div>
+  <div class="gallery">
+    <h1>{{ message }}</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    data () {
-        return {
-            message: 'This is a gallery component'
-        }
+  data () {
+    return {
+      message: 'This is a gallery component'
     }
+  }
 }
 </script>
 
