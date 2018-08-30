ee2b02dd467afdf75363dfc425319a5ec7ec3f2b
Sabrina Markon <phpsitescripts@outlook.com>
2018-08-29 23:04:47 -0600

    Create Gallery component

---
+++ src/components/Gallery.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="gallery">
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            message: 'This is a gallery component'
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
