7da12f71e9b87a5ce76ad29486e69d268fef8ac3
Sabrina Markon <phpsitescripts@outlook.com>
2018-09-01 00:23:10 -0600

    Add v-for Vue directive to display image thumbnails dynamically. Create key binding for the list.

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,55 +1,53 @@
 <template>
   <div class="gallery">
-    <div class="container">
-      <div class="row">
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img1.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img2.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img3.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img4.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img5.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img6.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img7.jpg" /></div>
-        </a>
-        <a href="#">
-          <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" src="https://vuejsbook.com/img/vuejs/img8.jpg" /></div>
-        </a>
-      </div>
-    </div>
+    <a v-for="{image, id} in images" v-bind:key="id" href="#">
+      <div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" v-bind:src="image.thumbnail" /></div>
+    </a>
   </div>
 </template>
 
 <script>
+/*
+** Please remember that: "If we want to display any image (loaded from our data) in Vue, we must use :src or v-bind:src"
+
+v-if - used to conditionally display or hide an element. If the condition is true, it will render the element.
+
+v-else - works just like v-if, but the condition is false.
+
+v-show - can be used instead of v-if. However, v-show always compiles and renders everything, and it just uses display:none (a CSS property) to hide the element.
+
+v-on - used to attach an event listener to an element.
+
+v-for - used to render a list of items.
+*/
+
 export default {
   data () {
     return {
-      message: 'This is a gallery component'
+      images: [
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img1.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img2.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img3.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img4.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img5.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img6.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img7.jpg' },
+        { thumbnail: 'https://vuejsbook.com/img/vuejs/img8.jpg' }
+      ]
     }
   }
 }
 </script>
 
 <style scoped>
-  img {
-    -webkit-box-shadow: 0px 1px 6px 1px rgba(0,0,0,0,0.75);
-    -moz-box-shadow: 0px 1px 6px 1px rgba(0,0,0,0,0.75);
-    box-shadow: 0px 1px 6px 1px rgba(0,0,0,0,0.75);
-    margin-bottom: 20px;
-  }
-  img:hover {
-    filter: gray; /* IE6-9 */
-    -webkit-filter: grayscale(1); /* Google Chrome, Safari 6+ & Opera 15+ */
-  }
+img {
+  -webkit-box-shadow: 0px 1px 6px 1px rgba(0, 0, 0, 0, 0.75);
+  -moz-box-shadow: 0px 1px 6px 1px rgba(0, 0, 0, 0, 0.75);
+  box-shadow: 0px 1px 6px 1px rgba(0, 0, 0, 0, 0.75);
+  margin-bottom: 20px;
+}
+img:hover {
+  filter: gray; /* IE6-9 */
+  -webkit-filter: grayscale(1); /* Google Chrome, Safari 6+ & Opera 15+ */
+}
 </style>
