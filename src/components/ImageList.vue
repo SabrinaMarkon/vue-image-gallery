9be16dd6864bcd96b60987540ac24e1bf35ead9b
Sabrina Markon <phpsitescripts@outlook.com>
2018-09-04 23:53:41 -0600

    Remove SingleImage component from ImageList and use emit to trigger a custom event

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <single-image :selectedImage="selectedImage"></single-image>
-    <a @click="showImage(image)" v-for="image in images" v-bind:key="image.id" href="#"><div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" :src="image.thumbnail" /></div></a>
+    <a @click="emitImage(image)" v-for="image in images" v-bind:key="image.id" href="#"><div class="col-md-3 col-sm-4 col-xs-6"><img class="img-responsive" :src="image.thumbnail" /></div></a>
   </div>
 </template>
 
@@ -11,15 +10,10 @@ import SingleImage from './SingleImage'
 export default {
   props: ['images'],
   components: {SingleImage},
-  data () {
-    return {
-      selectedImage: ''
-    }
-  },
   // In Vue, we can define a method in a component by using the methods option. We can add the methods option right below the data option
   methods: {
-    showImage: function (image) {
-      this.selectedImage = image
+    emitImage: function (image) {
+      this.$emit('emitImage', image)
     }
   }
 }
